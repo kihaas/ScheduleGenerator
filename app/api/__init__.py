224645afc4ab33bb5f93bfa6f82792dd53777f6d
kihaas <qwkihaas@gmail.com>
2025-09-24 17:44:46 +0400

    routes

--- app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.routes import subjects, schedule
-
-api_router = APIRouter()
-
-api_router.include_router(subjects.router, prefix="/api", tags=["subjects"])
-api_router.include_router(schedule.router, prefix="/api", tags=["schedule"])
--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -2,5 +2,7 @@ from fastapi import APIRouter
 from . import schedule, subjects
 
 api_router = APIRouter()
-api_router.include_router(schedule.router, prefix="/schedule", tags=["schedule"])
-api_router.include_router(subjects.router, prefix="/subjects", tags=["subjects"])
+
+# УБИРАЕМ префиксы! → теперь роуты будут доступны напрямую:
+api_router.include_router(schedule.router)        # → /generate-schedule
+api_router.include_router(subjects.router)
